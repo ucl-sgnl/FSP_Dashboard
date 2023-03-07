3027106930f0adcf7cda822fb9a4704d1a9994eb
blueriver212 <53503060+blueriver212@users.noreply.github.com>
2023-03-07 17:12:56 +0000

    removal of cesium to test for memory leak (#7)
    * removal of cesium to test for memory leak
    
    * removing all references to craco
    
    * removing all references to craco

--- src/scenes/globe/index.jsx
+++ src/scenes/globe/index.jsx
@@ -1,32 +1,6 @@
-import { Viewer, GeoJsonDataSource, PointPrimitiveCollection } from "resium";
-import { Color } from "cesium";
-
 const Globe = () => {
-
-  const data = {
-      type: "Feature",
-      properties: {
-        name: "Coors Field",
-        amenity: "Baseball Stadium",
-        popupContent: "This is where the Rockies play!",
-      },
-      geometry: {
-        type: "Point",
-        coordinates: [-104.99404, 39.75621],
-      },
-    };
-
-
-
   return (
-        <Viewer full>
-          <GeoJsonDataSource
-            data={data}
-            markerColor={Color.RED}
-            />
-            <PointPrimitiveCollection>
-            </PointPrimitiveCollection>
-        </Viewer>
+        <div> Hello </div>
   );
 }
 
