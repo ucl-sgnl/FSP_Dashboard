1051ca0c3f4ca16cf29c87548997c744938a7f89
blueriver212 <indy.brownhall@gmail.com>
2023-03-31 13:38:54 +0100

    updating the orbital view

--- src/scenes/globe/index.jsx
+++ src/scenes/globe/index.jsx
@@ -1,37 +1,6 @@
-import { Box, useTheme } from "@mui/material";
-import { tokens } from "../../theme";
-import { Viewer, Entity, GeoJsonDataSource, PointPrimitiveCollection } from "resium";
-import { Color } from "cesium";
-
 const Globe = () => {
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
-
-  const data = {
-      type: "Feature",
-      properties: {
-        name: "Coors Field",
-        amenity: "Baseball Stadium",
-        popupContent: "This is where the Rockies play!",
-      },
-      geometry: {
-        type: "Point",
-        coordinates: [-104.99404, 39.75621],
-      },
-    };
-
-
-
   return (
-        <Viewer full>
-          
-          <GeoJsonDataSource
-            data={data}
-            markerColor={Color.RED}
-            />
-            <PointPrimitiveCollection>
-            </PointPrimitiveCollection>
-        </Viewer>
+        <div>Hello</div>
   );
 }
 
